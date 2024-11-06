d19087ac12b9b0034747b2cb5c953b21afdd0275
uchithk130 <uchithk1302gmail.com>
2024-11-06 20:09:38 +0530

    Update

--- app/lib/mongoose.js
+++ app/lib/mongoose.js
@@ -6,7 +6,6 @@ const connectMongo = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
         });
         console.log("MongoDB connected successfully!");
     } catch (error) {
